27333fc39f71cbb2e5a6c3f9a544346475ac89ac
shrinidhi-xobin <shrinidhi.ramasubramanian@xobin.com>
2026-07-13 15:04:58 +0530

    Update urls_with_social.py

--- urls_with_social.py
+++ urls_with_social.py
@@ -1,6 +1,6 @@
 from django.urls import include, re_path
 from tcms.urls import urlpatterns as _tcms_urlpatterns
 
-urlpatterns = _tcms_urlpatterns + [
+urlpatterns = [
     re_path(r"^", include("social_django.urls", namespace="social")),
-]
+] + _tcms_urlpatterns
